7e419a7a990a022e26acd1ba9bf71c519ac2aaf8
Amir Zaidi <azaidi@live.nl>
2022-12-16 00:43:06 +0100

    Replace mipmaps with xml icon

--- app/src/main/java/amirz/btcodec/MainActivity.java
+++ app/src/main/java/amirz/btcodec/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends Activity implements BluetoothProfile.ServiceLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.app_name_long);
     }
 
     @Override
